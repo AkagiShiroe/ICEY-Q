93ed8b8710f098622e76ea9af55737b2c8a44b3d
AkagiShiroe <kirihaasuka@gmail.com>
2021-03-01 09:35:09 +0800

    ICEY-Q

--- build_Z00T.sh
+++ build_Z00T.sh
@@ -1,112 +1,48 @@
-#!/bin/bash
-BUILD_START=$(date +"%s")
+BUILD_START=$(date +"%s");
 
 # Colours
-blue='\033[0;34m'
-cyan='\033[0;36m'
-yellow='\033[0;33m'
-red='\033[0;31m'
-nocol='\033[0m'
+blue='\033[0;34m';
+cyan='\033[0;36m';
+yellow='\033[0;33m';
+red='\033[0;31m';
+nocol='\033[0m';
 
-# Kernel details
-KERNEL_NAME="FireKernel"
-VERSION="r3.2"
-DATE=$(date +"%d-%m-%Y-%I-%M")
-DEVICE="Z00T"
-FINAL_ZIP=$KERNEL_NAME-$VERSION-$DATE-$DEVICE.zip
-defconfig=Z00T_defconfig
+# Kernel details;
+KERNEL_NAME="ICEY-Q";
+VERSION="1.0a";
+DATE=$(date +"%d-%m-%Y-%I-%M");
+DEVICE="Z00T";
+OUT="msm8916";
+FINAL_ZIP=$KERNEL_NAME-$VERSION-$DATE-$DEVICE.zip;
 
 # Dirs
-BASE_DIR=/media/hdd/aayush/kernel
-KERNEL_DIR=$BASE_DIR/msm8916
-ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel3
-KERNEL_IMG=$KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
-UPLOAD_DIR=$BASE_DIR/$DEVICE
+ANYKERNEL_DIR=$TRAVIS_BUILD_DIR/AnyKernel3;
+KERNEL_IMG=$TRAVIS_BUILD_DIR/arch/arm64/boot/Image.gz-dtb;
+UPLOAD_DIR=$TRAVIS_BUILD_DIR/$OUT;
 
-# Export
-export ARCH=arm64
-export CROSS_COMPILE=/media/hdd/aayush/kernel/aarch64-elf-gcc/bin/aarch64-elf-
+git clone https://github.com/milouk/gcc-prebuilt-elf-toolchains.git -b master --single-branch --depth=1 -4 aarch64-milouk-elf -j$(nproc --all);
+export CROSS_COMPILE="/usr/bin/ccache ./aarch64-milouk-elf/aarch64-milouk-elf/bin/aarch64-milouk-elf-" ;
+export ARCH=arm64;
+export SUBARCH=arm;
+export KBUILD_BUILD_USER="Akanerin";
+export KBUILD_BUILD_HOST="Tsurara";
+STRIP="aarch64-milouk-elf/aarch64-milouk-elf/bin/aarch64-milouk-elf-strip";
+export CCOMPILE=$CROSS_COMPILE;
+export PATH=$PATH:./aarch64-milouk-elf/aarch64-milouk-elf/bin/ ;
+make Z00T_defconfig;
+make -j$(nproc --all);
+mkdir -p tmp_mod;
+make -j4 modules_install INSTALL_MOD_PATH=tmp_mod INSTALL_MOD_STRIP=1;
+find tmp_mod/ -name '*.ko' -type f -exec cp '{}' $ANYKERNEL_DIR/modules/system/lib/modules/ \;
+cp $KERNEL_IMG $ANYKERNEL_DIR;
+mkdir -p $UPLOAD_DIR;
+cd $ANYKERNEL_DIR;
+zip -r9 UPDATE-AnyKernel3.zip * -x README UPDATE-AnyKernel2.zip;
+mv $ANYKERNEL_DIR/UPDATE-AnyKernel3.zip $UPLOAD_DIR/$FINAL_ZIP;
 
-# Toolchain Used: https://developer.arm.com/open-source/gnu-toolchain/gnu-a/downloads
+# Cleanup
+rm $ANYKERNEL_DIR/Image.gz-dtb;
 
-## Functions ##
-
-# Make kernel
-function make_kernel() {
-  echo -e "$cyan***********************************************"
-  echo -e "          Initializing defconfig          "
-  echo -e "***********************************************$nocol"
-  make $defconfig
-  echo -e "$cyan***********************************************"
-  echo -e "             Building kernel          "
-  echo -e "***********************************************$nocol"
-  make -j8
-  if ! [ -a $KERNEL_IMG ];
-  then
-    echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
-    exit 1
-  fi
-}
-
-# Making zip
-function make_zip() {
-cp $KERNEL_IMG $ANYKERNEL_DIR
-mkdir -p $UPLOAD_DIR
-cd $ANYKERNEL_DIR
-zip -r9 UPDATE-AnyKernel2.zip * -x README UPDATE-AnyKernel2.zip
-mv $ANYKERNEL_DIR/UPDATE-AnyKernel2.zip $UPLOAD_DIR/$FINAL_ZIP
-}
-
-# Options
-function options() {
-echo -e "$cyan***********************************************"
-  echo "          Compiling FireKernel kernel          "
-  echo -e "***********************************************$nocol"
-  echo -e " "
-  echo -e " Select one of the following types of build : "
-  echo -e " 1.Dirty"
-  echo -e " 2.Clean"
-  echo -n " Your choice : ? "
-  read ch
-
-  echo -e " Select if you want zip or just kernel : "
-  echo -e " 1.Get flashable zip"
-  echo -e " 2.Get kernel only"
-  echo -n " Your choice : ? "
-  read ziporkernel
-
-case $ch in
-  1) echo -e "$cyan***********************************************"
-     echo -e "          	Dirty          "
-     echo -e "***********************************************$nocol"
-     make_kernel;;
-  2) echo -e "$cyan***********************************************"
-     echo -e "          	Clean          "
-     echo -e "***********************************************$nocol"
-     make clean
-     make mrproper
-     make_kernel;;
-esac
-
-if [ "$ziporkernel" = "1" ]; then
-     echo -e "$cyan***********************************************"
-     echo -e "     Making flashable zip        "
-     echo -e "***********************************************$nocol"
-     make_zip
-else
-     echo -e "$cyan***********************************************"
-     echo -e "     Building Kernel only        "
-     echo -e "***********************************************$nocol"
-fi
-}
-
-# Clean Up
-function cleanup(){
-rm -rf $ANYKERNEL_DIR/Image.gz-dtb
-}
-
-options
-cleanup
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+BUILD_END=$(date +"%s");
+DIFF=$(($BUILD_END - $BUILD_START));
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol";
